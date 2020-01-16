a9de1508625aeb44cda88d0578c0a5055248c1b4
unknown <safa.morjane@gmail.com>
2020-01-16 16:49:33 +0100

    Procedural Skybox + scene manager Script added

---
+++ Shader Graphs/Assets/Scripts/SceneManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour
+{
+
+    public Camera mainCam;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = mainCam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                if(hit.collider.tag.Equals("Dissolve"))
+                {
+                    MeshRenderer meshRenderer = hit.collider.gameObject.GetComponent<MeshRenderer>();
+                    int dissolve = meshRenderer.material.GetInt("_DissolveVariable");
+                    if (dissolve == -2)
+                    {
+                        dissolve = 0;
+                    }
+                    else
+                    {
+                        dissolve = -2;
+                    }
+                    meshRenderer.material.SetInt("_DissolveVariable",dissolve);
+                }
+            }
+        }
+    }
+}
